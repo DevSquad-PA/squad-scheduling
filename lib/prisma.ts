8a5c51a23ffb9a147ca8f85b6989a957dc9046bc
diogosalvadorb <diogosalvadorb@gmail.com>
2026-03-23 15:32:43 -0300

    fix: update import paths for Prisma client and schema configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@/server/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = `${process.env.DATABASE_URL}`;
